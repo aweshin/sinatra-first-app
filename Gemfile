eca480d3622204e59903b12e718c401be6fd3174
aweshin <aweshin.android@gmail.com>
2016-03-30 13:47:22 +0900

    形態素解析して作文する

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'twitter'
 gem 'rails_12factor', group: :production
+gem 'sinatra-contrib'
+gem 'natto'
--- app.rb
+++ app.rb
@@ -1,10 +1,15 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/reloader'
 require_relative 'tweet.rb'
 get '/' do
-	'under construction'
+	'under costruction'
 end
 
 # get '/random_tweet' do
 # 	Tweet.new.random_tweet
+# end
+
+# get '/random_tweet_using_mecab' do
+#     Tweet.new.random_tweet_using_mecab
 # end
