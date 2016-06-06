a84ef29e3007307805b3f2d501fddf811097687f
aweshin <aweshin.android@gmail.com>
2016-06-06 19:33:06 +0900

    tweet.rb modified

--- tweet.rb
+++ tweet.rb
@@ -9,6 +9,7 @@ SENTENCE_NO = [1-1..-1]
 
 WITH_MEDIA = ['─遺伝の世界とミームの世界の対応表─',
               'プロダクトデザイナー山中俊治氏の作品Ephyraは、極めて伸縮性の高いテキスタイルのロボット。Ephyraの触手は外界の環境を検知すると接触するかしないかという絶妙なタイミングで引っ込んでしまう。この動作はプログラムに従って動作しているにすぎないが、不思議と生命を感じさせる。']
+MEDIA = ['gene_meme.png', 'ephyra.png']
 
 class Tweet
   def initialize
@@ -43,11 +44,12 @@ class Tweet
     index = @text.index(@last_tweet)
     index = @random.rand(@text.size) unless index
     tweet = @text[(index + 1) % @text.size]
-    if WITH_MEDIA.include?(tweet)
+    if m_index = WITH_MEDIA.index(tweet)
       begin
-        @client.update_with_media(tweet, open('sample3.jpg'))
-      rescue => e
-        nil
+        @client.update_with_media(tweet, './photo/' + MEDIA[m_index])
+      rescue  => e
+        STDERR.puts "[EXCEPTION] " + e.to_s
+        exit 1
       end
     else
       update(@client, tweet)
@@ -85,7 +87,8 @@ class Tweet
       # tweet = (tweet.length > TWEET_LIMIT) ? tweet[0,TWEET_LIMIT] : tweet
       client.update(tweet.chomp)
     rescue => e
-      nil
+      STDERR.puts "[EXCEPTION] " + e.to_s
+      exit 1
     end
   end
 
