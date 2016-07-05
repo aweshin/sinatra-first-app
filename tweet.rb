41300627abc790b629a7c915323fdc4ee5fa9acf
aweshin <aweshin.android@gmail.com>
2016-07-05 19:42:13 +0900

    tweet.rb modified

--- tweet.rb
+++ tweet.rb
@@ -20,7 +20,7 @@ END_OF_MECAB_TWEET = ['なんてね。', 'とか言ってみる。', 'ふむふ�
                'からの経験の立ち上げ。', '…ああ。',
                'じっと手を見る。', 'ことばのカタルシス。', 'ちょっと危険。',
                'そっとささやく。', "#{(rand(1..100) ** 2) / 100}点。"]
-HASH_TAG = ' #ほぼ駄文ですが、たまにいいこと言います。'
+HASH_TAG = '#ほぼ駄文ですが'
 # メディアツイート
 WITH_MEDIA = ['遺伝の世界とミームの世界の対応表',
               'Wingsuits',
@@ -215,8 +215,8 @@ class Tweet
       tweets = check_limit(text)
       if tweets.instance_of?(Array) &&
         (ret = tweets[@random.rand(tweets.size)]).length <=
-        TWEET_LIMIT - END_OF_MECAB_TWEET.map{ |t| t.length }.max - HASH_TAG.length
-        return ret + END_OF_MECAB_TWEET.sample + HASH_TAG
+        TWEET_LIMIT - END_OF_MECAB_TWEET.map{ |t| t.length }.max - HASH_TAG.length - 1
+        return ret + END_OF_MECAB_TWEET.sample + "\n" + HASH_TAG
       end
     end
   end
