389b1cab257cf95b85bcb7ed987c3597df3a957a
aweshin <aweshin.android@gmail.com>
2016-07-07 17:41:01 +0900

    tweet.rb changed

--- tweet.rb
+++ tweet.rb
@@ -13,14 +13,14 @@ END_OF_THEME = '─'
 # MECAB_TWEETの連続数
 SEQUENCE_OF_MECAB_TWEET = (2..3).to_a.sample
 # テキストの取捨選択
-SENTENCE_NO = [32..42, 44..45, 62..68, 99..99, 106..106, 112..117, 139..-1]
+SENTENCE_NO = [32..42, 44..60, 62..68, 99..99, 106..106, 112..117, 139..-1]
 # mecabツイートの語尾
 END_OF_MECAB_TWEET = ['なんてね。', 'とか言ってみる。', 'ふむふむ…',
-               '(ry', '経験のパラレルワールド。', 'ちょっとしたファンタジー。',
+               '(ry', 'としてのパラレルワールド。', 'ちょっとしたファンタジー。',
                'からの経験の立ち上げ。', '…ああ。',
                'じっと手を見る。', 'ことばのカタルシス。', 'ちょっと危険。',
                'そっとささやく。', "#{(rand(1..100) ** 2) / 100}点。"]
-HASH_TAG = '　#ほぼ駄文ですが'
+HASH_TAG = ' #ほぼ駄文ですが'
 # メディアツイート
 WITH_MEDIA = ['遺伝の世界とミームの世界の対応表',
               'Wingsuits',
@@ -45,12 +45,13 @@ MEDIA = ['gene_meme.png',
 
 class Tweet
   def initialize
-    raw_text = File.open('./sentences.txt').read.split("\n")
+    sentences = File.open('./sentences.txt').read.split("\n")
 
-    @text = []
+    text = []
     SENTENCE_NO.each do |i|
-      @text += raw_text[i]
+      text += sentences[i]
     end
+    @text = join_text(text.flat_map{ |t| check_limit(t) })
 
     @client = Twitter::REST::Client.new(
       consumer_key:        ENV['TWITTER_CONSUMER_KEY'],
@@ -58,12 +59,6 @@ class Tweet
       access_token:        ENV['TWITTER_ACCESS_TOKEN'],
       access_token_secret: ENV['TWITTER_ACCESS_TOKEN_SECRET']
     )
-    @dic = {}
-    # ランダムインスタンスの生成
-    @random = Random.new
-    # 過去200件のツイートを取得
-    @last_200_tweets = @client.home_timeline(:count => 200)
-    @last_tweet = @last_200_tweets[0].text
   end
 
   def normal_tweet
@@ -78,14 +73,17 @@ class Tweet
       else
         update(tweet)
       end
+    else
+      update('もうだめぽ')
     end
   end
 
   # 形態素解析して作文する
   def random_tweet_using_mecab
     @text.shuffle!
-    make_dic
-    tweet = choice_sentence
+    dic = {}
+    make_dic(dic)
+    tweet = choice_sentence(dic)
     update(tweet)
   end
 
@@ -93,14 +91,12 @@ class Tweet
 
   # 最新TWEETがそのテーマの終わりならば、SEQUENCE_OF_MECAB_TWEET分MECAB_TWEETし、復帰
   def tweet_index
-    @text = @text.flat_map{ |t| check_limit(t) }
-    @text = join_text(@text)
-    indexes = @last_200_tweets[0,SEQUENCE_OF_MECAB_TWEET].map{ |tw|
+    indexes = @client.home_timeline(:count => SEQUENCE_OF_MECAB_TWEET).map{ |tw|
       tw = delete_https(tw.text)
       @text.index{ |t| t.include?(tw) }
     }
     # 分割ツイートで最新ツイートがメディアのみの場合を考慮
-    index = @last_tweet[-1].match(/。|！|？|─/) ? indexes[0] : indexes[1]
+    index = is_words?(@text[indexes[0]]) ? indexes[1] : indexes[0]
     unless index
       unless indexes.any?
         # 新しいテーマを決める
@@ -110,7 +106,7 @@ class Tweet
         return
       end
     else
-      if delete_https(@last_tweet)[-1] == END_OF_THEME
+      if delete_https(@text[indexes[0]])[-1] == END_OF_THEME
         random_tweet_using_mecab
         return
       end
@@ -138,28 +134,30 @@ class Tweet
     ret = [text.shift]
     text.each do |t|
       # 前の文章が句読点で終わっていなければ
-      unless ret[-1].match(/。|！|？|─/)
+      if is_words?(ret.last)
         loop do
           index = t.index(/。|！|？|─/)
           index = t.length - 1 unless index
           # 空文なら終了
           break if index == -1
-          #️⃣ 次の１文を足しても文字数が超過しなければ
+          # 次の１文を足しても文字数が超過しなければ
           if ret.last.length + index + 1 <= TWEET_LIMIT
-            if ret.last[-1].match(/。|！|？|─/)
-              ret[-1] += t.slice!(0, index + 1)
-            else
-              ret[-1] += "\n" + t.slice!(0, index + 1)
-            end
+            ret[-1] += is_words?(ret.last) ? "\n" + t.slice!(0, index + 1)
+              : t.slice!(0, index + 1)
           else
             ret << t
             break
           end
         end
-      else ret << t
+      else
+        ret << t
       end
     end
     ret
+  end
+
+  def is_words?(text)
+    !text[-1].match(/。|！|？|─/)
   end
 
   def delete_https(tweet)
@@ -170,7 +168,7 @@ class Tweet
     indexes = @text.map.with_index{ |t, i|
       i if delete_https(t)[-1] == END_OF_THEME }.compact
     # 最新200件にツイートされていないテーマを選ぶ
-    list = @last_200_tweets.map{ |tw|
+    list = @client.home_timeline(:count => 200).map{ |tw|
       tw = delete_https(tw.text)
       @text.index{ |t| t.include?(tw) } if tw[-1] == END_OF_THEME
     }.compact
@@ -203,9 +201,9 @@ class Tweet
   end
 
   # マルコフ連鎖用辞書の作成
-  def make_dic
+  def make_dic(dic)
     @text.each do |t|
-      t.gsub!(/「.+?」|（.+?）|─.+?──?|【.+?】|『.+?』/, '')
+      t.gsub!(/「.+?」。?|（.+?）|─.+?──?|【.+?】|『.+?』/, '')
     end
     nm = Natto::MeCab.new
     data = ['BEGIN','BEGIN']
@@ -220,33 +218,33 @@ class Tweet
     data.each_cons(3).each do |a|
       suffix = a.pop
       prefix = a
-      @dic[prefix] ||= []
-      @dic[prefix] << suffix
+      dic[prefix] ||= []
+      dic[prefix] << suffix
     end
   end
 
   # 辞書を元に作文を行う。文字数制限を加味する。
-  def choice_sentence
+  def choice_sentence(dic)
     loop do
-      text = connect
+      text = connect(dic)
       tweets = check_limit(text)
       if tweets.instance_of?(Array) &&
-        (ret = tweets[@random.rand(tweets.size)]).length <=
+        (ret = tweets[rand(tweets.size)]).length <=
         TWEET_LIMIT - END_OF_MECAB_TWEET.map{ |t| t.length }.max - HASH_TAG.length
         return ret + END_OF_MECAB_TWEET.sample + HASH_TAG
       end
     end
   end
 
-  def connect
+  def connect(dic)
     # スタートは begin,beginから
     prefix = ['BEGIN','BEGIN']
     ret = ''
     loop do
-      n = @dic[prefix].length
-      prefix = [prefix[1] , @dic[prefix][@random.rand(n)]]
+      n = dic[prefix].length
+      prefix = [prefix[1] , dic[prefix][rand(n)]]
       ret += prefix[0] if prefix[0] != 'BEGIN'
-      if @dic[prefix].last == 'END'
+      if dic[prefix].last == 'END'
         ret += prefix[1]
         break
       end
