b434a83cc88b42ebc4ca651cdcb1fe53d61c23f9
aweshin <aweshin.android@gmail.com>
2016-08-13 11:15:11 +0900

    db modified

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-require './models/sentence.rb'
-
-Sentence.order("id desc").all.each_with_index do |text, index|
-  Sentence.create(id: index + 1, sentence: text)
-end
--- models/sentence.rb
+++ models/sentence.rb
@@ -1,6 +1,7 @@
 require 'active_record'
 # データベースへの接続
-ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+ActiveRecord::Base.configurations = YAML.load_file('db/database.yml')
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || :development)
 
 
 class Sentence < ActiveRecord::Base
