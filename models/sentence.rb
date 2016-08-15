2718467960f5a043c71db3b852126acd7c448695
aweshin <aweshin.android@gmail.com>
2016-08-15 15:33:53 +0900

    models/sentence.rb changed

--- models/sentence.rb
+++ models/sentence.rb
@@ -1,7 +1,7 @@
 require 'active_record'
 # データベースへの接続
 ActiveRecord::Base.configurations = YAML.load_file('db/database.yml')
-ActiveRecord::Base.establish_connection(:development)
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || :development)
 
 
 class Sentence < ActiveRecord::Base
